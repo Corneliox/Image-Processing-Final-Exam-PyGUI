bb0557a77b76235b06c570df6e8573ec70726968
Cornelio <cornelioabdimash@gmail.com>
2024-12-15 16:54:40 +0700

    Update main.py
    Update
    - The main Interface
    - Fix Open all at begining
    - remove .os import--

--- New/src/main.py
+++ New/src/main.py
@@ -1,51 +1,49 @@
+# main.py
 import PySimpleGUI as sg
-import os
 import edgedetection
 import imagesegmentation
 import sharpening
 import smoothing
 
 def main():
-    # Layout utama untuk menu
     layout = [
-        [sg.Text('Main Menu', size=(20, 1), justification='center', font=('Helvetica', 25))],
-        [sg.Button('Edge Detection', size=(20, 2))],
-        [sg.Button('Image Segmentation', size=(20, 2))],
-        [sg.Button('Sharpening', size=(20, 2))],
-        [sg.Button('Smoothing', size=(20, 2))],
-        [sg.Button('Exit', size=(20, 2))]
+        [sg.Text('Welcome to Image Processing Suite', size=(30, 1), justification='center')],
+        [sg.Button('Edge Detection')],
+        [sg.Button('Image Segmentation')],
+        [sg.Button('Image Sharpening')],
+        [sg.Button('Image Smoothing')],
+        [sg.Button('Exit')]
     ]
 
-    window = sg.Window('Main Menu', layout, element_justification='center')
+    window = sg.Window('Main Menu', layout, finalize=True)
 
     while True:
-        event, values = window.read()
+        event, _ = window.read()
 
-        if event in (sg.WINDOW_CLOSED, 'Exit'):
+        if event in (sg.WIN_CLOSED, 'Exit'):
             break
 
-        # Navigation to other modules
         if event == 'Edge Detection':
             window.hide()
             edgedetection.main()
             window.un_hide()
 
-        elif event == 'Image Segmentation':
+        if event == 'Image Segmentation':
             window.hide()
             imagesegmentation.main()
             window.un_hide()
 
-        elif event == 'Sharpening':
+        if event == 'Image Sharpening':
             window.hide()
             sharpening.main()
             window.un_hide()
 
-        elif event == 'Smoothing':
+        if event == 'Image Smoothing':
             window.hide()
             smoothing.main()
             window.un_hide()
 
     window.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
